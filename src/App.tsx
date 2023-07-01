cb548db3d60784a7d38aad0be47fe488452150fb
Chetan <chetan.bhogade3899@gmail.com>
2023-07-01 23:35:49 +0530

    added constants

--- src/App.tsx
+++ src/App.tsx
@@ -6,112 +6,41 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {ColorCodes} from './constants/common';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.root}>
+      <StatusBar backgroundColor={'#ecf0f1'} barStyle={'dark-content'} />
+      <View style={styles.container}>
+        <Text style={styles.heading}>Simple Form</Text>
+        <View style={styles.divider} />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  divider: {
+    width: '100%',
+    borderTopWidth: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
+  heading: {
+    color: ColorCodes.black,
+    fontSize: 26,
+    marginVertical: 16,
+    textAlign: 'center',
     fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    borderWidth: 1,
   },
-  highlight: {
-    fontWeight: '700',
+  root: {
+    flex: 1,
+    backgroundColor: '#ecf0f1',
   },
 });
 
---
+++ src/constants/common.js
@@ -0,0 +1,37 @@
+export const ColorCodes = {
+  aliceblue: '#f0f8ff',
+  antiquewhite: '#faebd7',
+  aqua: '#00ffff',
+  aquamarine: '#7fffd4',
+  azure: '#f0ffff',
+  beige: '#f5f5dc',
+  bisque: '#ffe4c4',
+  black: '#000000',
+  blanchedalmond: '#ffebcd',
+  blue: '#0000ff',
+  blueviolet: '#8a2be2',
+  brown: '#a52a2a',
+};
+
+export const zipCodes = [
+  {
+    zipcode: '110011',
+    city: 'Central Delhi',
+    state: 'ND',
+  },
+  {
+    zipcode: '400011',
+    city: 'Mumbai',
+    state: 'MH',
+  },
+  {
+    zipcode: '600001',
+    city: 'Chennai',
+    state: 'TN',
+  },
+  {
+    zipcode: '700001',
+    city: 'Kolkata',
+    state: 'WB',
+  },
+];
